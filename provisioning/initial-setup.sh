f63d9fb146adba05ea10a2e3363b76e432c1dd4e
ChiperSoft <chiper@chipersoft.com>
2014-03-17 20:16:47 -0700

    Cleaning up provisioning.

--- provisioning/initial-setup.sh
+++ provisioning/initial-setup.sh
@@ -4,3 +4,12 @@ cat "/vagrant/provisioning/self-promotion.txt"
 
 echo "Copying bashrc..."
 cp /vagrant/provisioning/files/bashrc /home/vagrant/.bashrc
+
+echo "Seeding mongodb..."
+mongorestore /vagrant/provisioning/seeds/mongodb/
+
+echo "Seeding redis..."
+service redis-server stop
+cp /vagrant/provisioning/seeds/redis/dump.rdb /var/lib/redis/dump.rdb
+chown redis:redis /var/lib/redis/dump.rdb
+service redis-server start
