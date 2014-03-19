71d0f2affd26aa6528ce63a1e4cc0e0af89e883b
ChiperSoft <chiper@chipersoft.com>
2014-03-18 17:33:40 -0700

    Songbar now sizes its scrolling to fit either space to bottom of the window,  or top of the footer (whichever is smaller).

--- srv/public/assets/components/songbar/main.js
+++ srv/public/assets/components/songbar/main.js
@@ -3,6 +3,7 @@ define(['lodash', 'backbone', 'events', 'models/Track', './songDetails.hbs'], fu
 
 	return Backbone.View.extend({
 		template: tmplDetails,
+		lastHeight: 0,
 
 		initialize: function () {
 			var json = this.$('script.track-data').html();
@@ -20,6 +21,36 @@ define(['lodash', 'backbone', 'events', 'models/Track', './songDetails.hbs'], fu
 			this.render = this.render.bind(this);
 
 			events.on('track', this.onTrackEvent, this);
+
+			$(window).on('resize scroll', this.onPageScroll.bind(this));
+			this.updateDetailHeight();
+			
+		},
+
+		onPageScroll: function (ev) {
+			// no need to update the view height if the drawer is hidden
+			if (!this.model) {return;}
+			this.updateDetailHeight();
+		},
+
+		// Find the space between the top of the details view and both the bottom of the window
+		// or the top of the footer, and sets the details view height to the smaller of the two.
+		// This makes sure the view has a scrollbar when needed.
+		updateDetailHeight: function (force) {
+			var pageScroll = $(window).scrollTop(),
+				$details = this.$('.details'),
+				detailsTop = $details.position().top + this.$('.body').position().top,
+				footerTop = $('footer').offset().top,
+				viewHeight = $(window).height(),
+				height = Math.min(footerTop - pageScroll - detailsTop, viewHeight - detailsTop)
+			;
+
+			// check for changes so that we can avoid unnecessary redraws
+			if (force || height !== this.lastHeight) {
+				this.lastHeight = height;
+				$details.css('height', height);
+			}
+
 		},
 
 		onTrackEvent: function (event, id) {
@@ -31,6 +62,8 @@ define(['lodash', 'backbone', 'events', 'models/Track', './songDetails.hbs'], fu
 				this.model = new Track({id: id});
 				this.listenTo(this.model, 'sync', this.render);
 				this.render();
+			} else if (!id) {
+				this.model = null;
 			}
 		},
 
@@ -40,6 +73,8 @@ define(['lodash', 'backbone', 'events', 'models/Track', './songDetails.hbs'], fu
 			var html = this.template(data);
 
 			this.$('.body > div').html(html);
+
+			this.updateDetailHeight(true);
 
 			return this;
 		}
