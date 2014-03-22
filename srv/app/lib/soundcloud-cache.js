82dbc862cc51a0ec9a5c7aa8b3d0abf8fa9e5cdd
ChiperSoft <chiper@chipersoft.com>
2014-03-22 13:52:35 -0700

    Minor code cleanup, and added comments. jshint no longer complains about function order.

--- srv/app/lib/soundcloud-cache.js
+++ srv/app/lib/soundcloud-cache.js
@@ -1,30 +1,111 @@
+
+// This module works by taking advantage of a feature of promises, that if a promise is already resolved
+// then it will instantly call any onResolved function that gets added with .then().  Any time a request
+// is made for a track we don't have yet, we store the deferral that gets created for the request and
+// reuse that deferral's promise for all subsequent requests of the same track id.
+
+// This module performs request pooling. The module is invoked for single tracks, but to avoid making
+// dozens of individual http requests, we collect multiple ids together and request them all at once.
+// This is done by putting the actual http request on a setTimeout of 30ms.  The first request kicks
+// off the timer and until that timer expires all other calls to make the request are ignored.
+
+// load dependencies and the app configuration
 var request = require('request');
 var _ = require('lodash');
 var when = require('when');
 var config = require('app/config');
 
-var URL_LIMIT = 1000;
+// define constants
+var URL_LIMIT = 1900;
 var CACHE_TIMEOUT = 1000 * 60 * 60 * 6; //six hours
-var cache = {};
+var DEBOUNCE_TIMER = 30;
+var MAX_TRACKS = 40;
 
+var cache = {};
 var pending = {};
-var pendingCount = 0;
 var fetching;
 
-function getTags(string) {
-	var tags = [], tag;
-	var regexp = /[^\s"']+|"([^"]*)"/g;
-	while (tag = regexp.exec(string)) {
-		tag = tag[1] || tag[0];
-		if (tag.toUpperCase() !== 'MASHUP') {
-			tags.push(tag);
-		}
+module.exports = function (id) {
+	// if we already have a stored promise for this cache item, and the promise
+	// wasn't created more than CACHE_TIMEOUT ago, return it.
+	if (cache[id] && Date.now() - cache[id].created < CACHE_TIMEOUT ) {
+		return cache[id].promise;
 	}
-	return tags;
+
+	// create a new deferral and store it in the cache, with a creation date
+	cache[id] = when.defer();
+	cache[id].created = Date.now();
+
+	// add the request id to the pending requests collection since we don't want to request the
+	// same id multiple times, we use an as a quick and dirty unique table
+	pending[id] = true;
+
+	// kick off the request timer
+	fetchDebounce();
+
+	return cache[id].promise;
+};
+
+
+function fetchDebounce() {
+	// a fetch is already pending, just ignore the call
+	if (fetching) {return;}
+
+	fetching = setTimeout(fetch, DEBOUNCE_TIMER);
+}
+
+function fetch () {
+	fetching = false;
+
+	// get an array of all the track ids we need to lookup
+	var ids = _.keys(pending);
+
+	while (ids.length) {
+		makeRequest();
+	}
+
+	// There is a physical limit to the maximum length of a url (roughly 2000 characters).  In practical use
+	// we should never be requesting enough tracks to overflow this (that'd be about 175 ids at once), but
+	// one never knows...  Additionally, soundcloud's docs are unclear about how many tracks we can request
+	// at once. Searches default to 50 results per page, but for safety sake we cap it at 40 (MAX_TRACKS).
+
+	function makeRequest () {
+		var id,
+			expecting = {},
+			trackCount = 0,
+			url = 'https://api.soundcloud.com/tracks.json?client_id='+config.soundcloudKey+'&ids=';
+
+		do {
+			// pluck an id off the stack
+			id = ids.shift();
+
+			// record that we're expecting that id in the response. We do this because if a track is deleted,
+			// soundcloud doesn't tell us, it just doesn't include that track in the results.
+			expecting[id] = true;
+
+			// remove this id from the pending table
+			delete pending[id];
+
+			// append the id to the url
+			url += ',' + id;
+
+			// count how many tracks we've added to this request
+			trackCount++;
+
+		} while (trackCount < MAX_TRACKS && url.length < URL_LIMIT && ids.length);
+
+		// make the http request.  pass the results, along with what tracks we were expecting from this request,
+		// to the response handler.
+		request.get(url, {json:true}, function (err, response, tracks) {
+			processResponse(err, tracks, expecting);
+		});
+	}
+
 }
 
 
 function processResponse (err, tracks, expecting) {
+	// if the request failed, reject all the track promises for tracks we were expecting in that request
 	if (err) {
 		console.error(err);
 		_.each(expecting, function (junk, id) {
@@ -33,9 +114,13 @@ function processResponse (err, tracks, expecting) {
 		return;
 	}
 
+	// for each track we get back...
 	_.each(tracks, function (track) {
 
+		// parse the tags list into something usable
 		track.tags = track.tag_list && getTags(track.tag_list) || track.tags || [];
+
+		// convert the artwork url to the largest size we need (for the songbar)
 		if (track.artwork_url) {
 			track.artwork_url = track.artwork_url.replace('-large', '-t300x300');
 		}
@@ -45,12 +130,14 @@ function processResponse (err, tracks, expecting) {
 			console.error('Received track we did not request.', track);
 		}
 
+		// remove this track from the expecting list
 		delete expecting[track.id];
 
+		// resolve the promise for this track
 		defer.resolve(track);
 	});
 
-	// loop through any remaining expected tracks and do false callbacks to indicate track was not found.
+	// loop through any remaining expected tracks and resolve with false to indicate track was not found.
 	_.each(expecting, function (junk, id) {
 		var defer = cache[id];
 		defer.resolve(false);
@@ -58,58 +145,17 @@ function processResponse (err, tracks, expecting) {
 
 }
 
-function fetch () {
-	fetching = false;
+function getTags(string) {
+	var tags = [], tag;
+	var regexp = /[^\s"']+|"([^"]*)"/g;
+	while (tag = regexp.exec(string)) {
+		tag = tag[1] || tag[0];
 
-	var ids = _.keys(pending);
-
-	function makeRequest () {
-		var id = ids.shift(),
-			expecting = {},
-			url = 'https://api.soundcloud.com/tracks.json?client_id='+config.soundcloudKey+'&ids=' + id;
-
-		expecting[id] = true;
-		delete pending[id];
-
-		while (url.length < URL_LIMIT && ids.length) {
-			id = ids.shift();
-			expecting[id] = true;
-			delete pending[id];
-			url += ',' + id;
+		// filter the mashup tag, since its implied
+		if (tag.toUpperCase() !== 'MASHUP') {
+			tags.push(tag);
 		}
-
-		request.get(url, {json:true}, function (err, response, tracks) {
-			processResponse(err, tracks, expecting);
-		});
 	}
-
-	while (ids.length) {
-		makeRequest();
-	}
+	return tags;
 }
 
-function fetchDebounce() {
-	// a fetch is already pending, just ignore
-	if (fetching) {return;}
-
-	fetching = setTimeout(fetch, 30);
-}
-
-module.exports = function (id) {
-	// if we already have a stored promise for this cache item, and the promise
-	// wasn't created more than CACHE_TIMEOUT ago, return it. Otherwise, re-download
-	// the track data.
-	if (cache[id] && Date.now() - cache[id].created < CACHE_TIMEOUT ) {
-		return cache[id].promise;
-	}
-
-	cache[id] = when.defer();
-	cache[id].created = Date.now();
-
-	pending[id] = true;
-	pendingCount++;
-
-	fetchDebounce();
-
-	return cache[id].promise;
-};
--- srv/site.js
+++ srv/site.js
@@ -1,35 +1,48 @@
 var env = process.env.NODE_ENV || 'development';
+var isProduction = env === 'production';
 
-if (env === 'production') {
+// activate the newrelic process monitoring
+if (isProduction) {
 	require('newrelic');
 }
 
+// setup the when.js debugging for unhandled promise rejections
+// eventually this should be made development only, but the site is still kinda beta
+require('when/monitor/console');
+
+// load express and any middleware dependencies that need to be saved
 var express = require('express');
 var expressSession = require("express-session");
 var expressSessionRedisStore = require('connect-redis')(expressSession);
 
-
-require('when/monitor/console');
-
+// load local modules
 var config = require('app/config');
 var log = require('app/log');
+
+// load our database connections
 var mongo = require('app/db/mongo');
 var redis = require('app/db/redis');
 
 var app = express();
-
-app.set('view', require('app/lib/view'));
-app.set('views', __dirname + '/views/');
 app.set('port', process.env.PORT || 8000);
 
+// initialize our custom view renderer
+app.set('view', require('app/lib/view'));
+app.set('views', __dirname + '/views/');
+
+// register any local variables that are application-wide
 app.locals.soundcloudKey = config.soundcloudKey;
 
+// first middleware is the request logger
 app.use(require('app/middleware/logger')());
 
-app.use(require('response-time')());
+// setup middleware for static assets. In production these should never be hit,
+// since nginx serves them, but we still register them before any other middleware
+// so that static assets don't invoke the session code
 app.use(require('static-favicon')(__dirname + '/public/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/public/assets'));
 
+// process the request cookies and register the session handler with a redis store
 app.use(require('cookie-parser')());
 app.use(expressSession({
 	store: new expressSessionRedisStore({ client: redis }),
@@ -37,18 +50,26 @@ app.use(expressSession({
 	key: config.sessions.cookieKey
 }));
 
+// register any request specific locals.
 app.use(function(req, res, next){
 	res.locals.httproot = req.protocol + '://' + req.host;
 	next();
 });
 
+// with all middleware in place, register the actual routers that handle the page requests.
 app.use(require('app/routes/main')());
 
+// if our site were able to have 404 errors, the handler for that would go here.
+// mash.li only 404s when requesting specific tracks that don't exist, which is handled in
+// the main router.
+
+// finally, register an error handler at the tail end of the request.
 app.use(require('errorhandler')({
 	dumpExceptions:true,
 	showStack:true
 }));
 
+// start the server.
 var server = app.listen(app.get('port'), function() {
 	log({
 		level: 1,
@@ -58,7 +79,8 @@ var server = app.listen(app.get('port'), function() {
 	});
 });
 
-if (env === 'production') {
+// if we're on production, setup a sigterm hook so that the server can be gracefully terminated
+if (isProduction) {
 	process.on('SIGTERM', function () {
 		log({
 			level: 1,
