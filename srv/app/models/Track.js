3f5ce196ae0b88aeafcf6371b4ed79ded0977b7f
ChiperSoft <chiper@chipersoft.com>
2014-03-30 09:02:59 -0700

    Refactored some code duplication with populating soundcloud data.

--- srv/app/models/Track.js
+++ srv/app/models/Track.js
@@ -35,27 +35,41 @@ Track.promiseTrackByID = function (id, asModel) {
 	}
 
 	return p.then(function (model) {
-		if (!model) return false;
+		if (!model) {return false;}
 		
-		var track = model.toObject();
-		return promiseFromSoundcloudCache(track._id).then(
-			function (details) {
-				// if details is false, that means soundcloud no longer has the track
-				// return false in that situation, since the track no longer exists
-				if (details) {
-					track.details = details;
-					return track;
-				} else {
-					return false;
-				}
-			},
-			// if the promise rejected, that means we had an error communicating with
-			// soundcloud. This could mean they're offline, so we return what we stored at scan time
-			function () {
+		return model.promisePopulatedFromSoundcloud();
+	});
+};
+
+Track.prototype.promisePopulatedFromSoundcloud = function () {
+	var track = this.toObject();
+	track.score = this.getScore();
+	delete track.votesActual;
+	return promiseFromSoundcloudCache(track._id).then(
+		function (details) {
+			// if details is false, that means soundcloud no longer has the track
+			// return false in that situation, since the track no longer exists
+			if (details) {
+				track.details = details;
 				return track;
+			} else {
+				return false;
 			}
-		);
-	})
+		},
+		// if the promise rejected, that means we had an error communicating with
+		// soundcloud. This could mean they're offline, so we return what we stored at scan time
+		function () {
+			return track;
+		}
+	);
+};
+
+Track.prototype.getScore = function () {
+	return 1 + this.votes[1] - this.votes[-1];
+};
+
+Track.prototype.getActualScore = function () {
+	return 1 + this.votesActual[1] - this.votesActual[-1];
 };
 
 module.exports = Track;
--- srv/app/models/TrackList.js
+++ srv/app/models/TrackList.js
@@ -5,8 +5,6 @@ var when = require('when');
 var mongoose = require('app/db/mongo');
 var Track = require('app/models/Track');
 
-var promiseSoundcloudDetails = require('app/lib/soundcloud-cache');
-
 var sTrackList = mongoose.Schema({
 	_id: String,
 	tracks: [{ type: String, ref: 'Track' }],
@@ -53,24 +51,7 @@ TrackList.promiseTrackList = function (name, start, limit, asModels) {
 		}
 
 		return when.map(tracklist.tracks, function (model) {
-			var track = model.toObject();
-			return promiseSoundcloudDetails(track._id).then(
-				function (details) {
-					// if details is false, that means soundcloud no longer has the track
-					// return false in that situation, since the track no longer exists
-					if (details) {
-						track.details = details;
-						return track;
-					} else {
-						return false;
-					}
-				},
-				// if the promise rejected, that means we had an error communicating with
-				// soundcloud. This could mean they're offline, so we return what we stored at scan time
-				function () {
-					return track;
-				}
-			);
+			return model.promisePopulatedFromSoundcloud();
 		});
 	});
 
@@ -91,24 +72,7 @@ TrackList.promiseTrackList['new'] = function (start, limit, asModels) {
 	}
 
 	return when.map(when(p), function (model) {
-		var track = model.toObject();
-		return promiseSoundcloudDetails(track._id).then(
-			function (details) {
-				// if details is false, that means soundcloud no longer has the track
-				// return false in that situation, since the track no longer exists
-				if (details) {
-					track.details = details;
-					return track;
-				} else {
-					return false;
-				}
-			},
-			// if the promise rejected, that means we had an error communicating with
-			// soundcloud. This could mean they're offline, so we return what we stored at scan time
-			function () {
-				return track;
-			}
-		);
+		return model.promisePopulatedFromSoundcloud();
 	});
 };
 
