e93caecefd920e9e5a0f059c356e439c78e3bde2
ChiperSoft <chiper@chipersoft.com>
2014-05-03 22:35:48 -0700

    Require visitor cookie for downloads.

--- srv/app/routes/download.js
+++ srv/app/routes/download.js
@@ -9,6 +9,8 @@ module.exports = exports = function () {
 
 	router.param('trackid', require('app/middleware/loadTrack')());
 
+	router.use(require('app/middleware/visitor').loader);
+
 	//setup endpoints
 	router.get('/download/:trackid', exports.downloadTrack);
 
@@ -19,6 +21,20 @@ module.exports = exports = function () {
 
 exports.downloadTrack = function (req, res, next) {
 	var track = res.locals.track;
+
+	// Verify the visitor cookie exists
+	if (!res.locals.visitorid) {
+		if (res.locals.wantsJSON) {
+			res.json(403, { error: { message: 'Expected visitor cookie was not found.' }});
+		} else {
+			res.status(403);
+			res.locals.error = 'You may only download from the front page';
+			res.locals.statusCode = 403;
+			res.render('pages/error', res.locals);
+		}
+		return;
+	}
+
 	promiseFromSoundcloudCache(track.id).then(function (details) {
 		if (!details.downloadable || !details.download_url) {
 			if (res.locals.wantsJSON) {
